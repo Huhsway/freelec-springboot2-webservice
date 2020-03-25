2c5d70830f2d29afea58c9bb270e06d84bf9a509
Huhsway <seunghoe9542@naver.com>
2020-03-25 22:42:35 +0900

    4장까지

--- src/main/java/com/jojoldu/book/springboot/Application.java
+++ src/main/java/com/jojoldu/book/springboot/Application.java
@@ -3,7 +3,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing // JPA Auditing 활성화
+// @EnableJpaAuditing // JPA Auditing 활성화
 @SpringBootApplication // 이 설정으로 인해 스프링 부트의 자동 설정, 스프링 Bean 읽기와 생성 모두 자동으로 설정된다.
 // 특히 @SpringBootApplication이 있는 위치부터 설정을 읽어가기 떄문에 이 클래스는 항상 프로젝트의 최상단에 위치해야만 한다.
 public class Application { // 이 클래스가 이제 메인 클래스가 된다.
--- src/main/java/com/jojoldu/book/springboot/web/IndexController.java
+++ src/main/java/com/jojoldu/book/springboot/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.jojoldu.book.springboot.web;
 
+import com.jojoldu.book.springboot.config.auth.LoginUser;
+import com.jojoldu.book.springboot.config.auth.dto.SessionUser;
 import com.jojoldu.book.springboot.service.PostsService;
 import com.jojoldu.book.springboot.web.dto.PostsResponseDto;
 import com.jojoldu.book.springboot.web.dto.PostsSaveRequestDto;
@@ -9,6 +11,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
@@ -16,8 +20,15 @@ public class IndexController {
     private final PostsService postsService;
 
     @GetMapping("/")
-    public String index(Model model) { // Model은 서버 템플릿 엔진에서 사용할 수 있는 객체를 저장할 수 있다. 여기서는 postsService.findAllDesc()로 가져온 겨로가를 posts로 index.mustache에 전달함
+    public String index(Model model, @LoginUser SessionUser user) { // Model은 서버 템플릿 엔진에서 사용할 수 있는 객체를 저장할 수 있다. 여기서는 postsService.findAllDesc()로 가져온 겨로가를 posts로 index.mustache에 전달함
+        // @LoginUser SessionUser user은 기존에 (user) httpSesseion.getAttribute("user")로 가져오던 세션 정보 값이 개선됨
+        // 이제는 어느 컨트롤러든지 @LoginUser만 사용하면 세션 정보를 가져올 수 있게 됨
         model.addAttribute("posts", postsService.findAllDesc());
+        // SessionUser user = (SessionUser) httpSession.getAttribute("user"); // 앞서 작성된 CustomOAuth2UserService에서 로그인 성공시 세션에 SessionUser를 저장하도록 구성했다.
+        // 즉 로그인 성공시 httpSession.getAttribute("user")에서 값을 가져올 수 있다.
+        if (user != null) { // 세션에 저장된 값이 있을때만 model에 userName으로 등록한다. 세션에 저장된 값이 없으면 model엔 아무른 값이 없는 상태이니 로그인 버튼이 보이게 된다.
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
