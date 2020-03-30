b7914610b7cc613e797b6cf8a7ed5bae67dc07c5
Huhsway <seunghoe9542@naver.com>
2020-03-30 16:32:55 +0900

    5장까지

--- src/main/java/com/jojoldu/book/springboot/Application.java
+++ src/main/java/com/jojoldu/book/springboot/Application.java
@@ -1,7 +1,7 @@
 package com.jojoldu.book.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+// import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 // @EnableJpaAuditing // JPA Auditing 활성화
 @SpringBootApplication // 이 설정으로 인해 스프링 부트의 자동 설정, 스프링 Bean 읽기와 생성 모두 자동으로 설정된다.
