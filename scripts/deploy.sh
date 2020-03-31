e74795c2381d436aa81cfda2240b39da047750ef
Huhsway <seunghoe9542@naver.com>
2020-03-31 20:01:51 +0900

    배포 자동화 구성

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 
 REPOSITORY=/home/ec2-user/app/step2
@@ -38,5 +37,6 @@ echo "> $JAR_NAME 실행"
 nohup java -jar \
     -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
     -Dspring.profiles.active=real \
-    $JAR_NAME > $REPOSITORY/nohup.out 2>&1 & # nohup 실행 시 CodeDeploy는 무한 대기한다. 이 이슈를 해결하기 위해 nohup.out 파일을 표준 입출력용으로 별도로 사용 이렇게 하지 않으면 nohup.out 파일이 생기지 않고, CodeDeploy 로그에 표준 입출력이 출력 됨
+    $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+    # nohup 실행 시 CodeDeploy는 무한 대기한다. 이 이슈를 해결하기 위해 nohup.out 파일을 표준 입출력용으로 별도로 사용 이렇게 하지 않으면 nohup.out 파일이 생기지 않고, CodeDeploy 로그에 표준 입출력이 출력 됨
     # nohup이 끝나기 전까지 CodeDeploy도 끝나지 않으니 꼭 이렇게 해아만 한다.
